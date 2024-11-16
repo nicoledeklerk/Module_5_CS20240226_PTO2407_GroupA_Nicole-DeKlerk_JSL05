b434f2963b5f4a6ab306b0639e06a82a0a6d51a7
nicoledeklerk <nicolenataliedeklerk@gmail.com>
2024-11-16 17:50:16 +0200

    Expand object
    Included three more guardians and their preferred genre's to the 'guardian' object

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Drax": "Country",
+    "Rocket": "Indie/Alternative",
+    "Groot": "R&B"
     // Add preferences for Drax, Rocket, and Groot
 };
 
